0d761f9104dfd166ce589378a447272027305d0a
reddd3242 <redvelvedr@gmail.com>
2025-06-01 22:16:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# 🌍 Dış dünyaya açmak için şu satırı en alta ekle:
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
